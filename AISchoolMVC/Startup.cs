f3309d21c6d1d66784c28f628960bbec12967508
Auwal <engr.auwal@gmail.com>
2020-06-05 20:09:49 +0100

    Add project files.

---
+++ AISchoolMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AISchoolMVC.Startup))]
+namespace AISchoolMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
